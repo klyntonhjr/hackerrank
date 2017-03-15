693d3b4ee1d224e56914dbea9a988723aaac1bf9
Marklyton Holmes <marklytonh@gmail.com>
2017-03-15 00:00:57 -0400

    Challenge Completed: Welcome to Java!

---
+++ Java/src/main/java/com/marklytonh/welcometojava/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.marklytonh.welcometojava;
+
+/**
+ * Created by nino on 3/14/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
---
+++ Java/src/test/java/com/marklytonh/welcometojava/HelloWorldTest.java
@@ -0,0 +1,36 @@
+package com.marklytonh.welcometojava;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by nino on 3/14/17.
+ */
+public class HelloWorldTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void helloWorldOutputTest(){
+        final String EXPECTED = "Hello, World.\nHello, Java.";
+        String[] args = null;
+        HelloWorld.main(args);
+
+        assertEquals(EXPECTED, outContent.toString().trim());
+    }
+}
