601b48092cdd11ab1830e98a92b6ae94c9b60567
Marklyton Holmes <marklytonh@gmail.com>
2017-03-16 10:30:49 -0400

    Challenge Completed: Java > Introduction > Java If-Else

---
+++ Java/src/test/java/challenges/introduction/javaifelse/JavaIfElseTest.java
@@ -0,0 +1,74 @@
+package challenges.introduction.javaifelse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by nino on 3/15/17.
+ */
+public class JavaIfElseTest {
+    private final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+    private final InputStream stdin = System.in;
+    // All data inputs are in range 1 <= n <= 100
+    private String dataInput;
+    private String EXPECTED;
+    private final String[] args = null;
+
+    @Before
+    public void setUp(){
+        System.setOut(new PrintStream(stdout));
+        System.setIn(stdin);
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(null);
+        System.setIn(null);
+        dataInput = null;
+        EXPECTED = null;
+    }
+
+    @Test
+    public void ifOddPrintWeirdTest(){
+        dataInput = "3";
+        EXPECTED = "Weird";
+        System.setIn(new ByteArrayInputStream(dataInput.getBytes()));
+        JavaIfElse.main(args);
+        assertEquals(EXPECTED, stdout.toString().trim());
+    }
+
+    @Test
+    public void ifEvenAndBetween2And5PrintNotWeirdTest(){
+        dataInput = "2";
+        EXPECTED = "Not Weird";
+        System.setIn(new ByteArrayInputStream(dataInput.getBytes()));
+        JavaIfElse.main(args);
+        assertEquals(EXPECTED, stdout.toString().trim());
+    }
+
+    @Test
+    public void ifEvenAndBetween6And20PrintWeirdTest(){
+        dataInput = "6";
+        EXPECTED = "Weird";
+        System.setIn(new ByteArrayInputStream(dataInput.getBytes()));
+        JavaIfElse.main(args);
+        assertEquals(EXPECTED, stdout.toString().trim());
+    }
+
+    @Test
+    public void ifEvenAndGreaterThan20PrintWeirdTest(){
+        dataInput = "22";
+        EXPECTED = "Not Weird";
+        System.setIn(new ByteArrayInputStream(dataInput.getBytes()));
+        JavaIfElse.main(args);
+        assertEquals(EXPECTED, stdout.toString().trim());
+    }
+}
