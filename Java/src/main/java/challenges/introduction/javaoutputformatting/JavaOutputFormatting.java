e633dfff3b3e7921c5a0f5b0ccb0c9a7fbbea5e8
Marklyton Holmes <marklytonh@gmail.com>
2017-03-16 20:26:36 -0400

    Challenge Completed: Java > Introduciton > Java Output Formatting

---
+++ Java/src/main/java/challenges/introduction/javaoutputformatting/JavaOutputFormatting.java
@@ -0,0 +1,20 @@
+package challenges.introduction.javaoutputformatting;
+
+import java.util.Scanner;
+
+/**
+ * Created by nino on 3/16/17.
+ */
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(
+                "================================\n" +
+                String.format("%-15s%03d\n", scan.next(), scan.nextInt()) +
+                String.format("%-15s%03d\n", scan.next(), scan.nextInt()) +
+                String.format("%-15s%03d\n", scan.next(), scan.nextInt()) +
+                "================================"
+        );
+    }
+}
