9d7651612d8f08d4279b037dba75953283c1da5b
Marklyton Holmes <marklytonh@gmail.com>
2017-03-19 00:38:00 -0400

    Challenge Completed: Java > Introduction > Java Date and Time

---
+++ Java/src/main/java/challenges/introduction/javadateandtime/JavaDateAndTime.java
@@ -0,0 +1,44 @@
+package challenges.introduction.javadateandtime;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ * Created by nino on 3/18/17.
+ */
+public class JavaDateAndTime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt()-1; // Adjust for Calendar class starting its months at 0.
+        int day = sc.nextInt();
+        int year = sc.nextInt();
+
+        int dayOfWk = new Calendar
+                .Builder()
+                .setDate(year, month, day)
+                .build()
+                .get(Calendar.DAY_OF_WEEK);
+        System.out.print(getDay(dayOfWk));
+    }
+
+    public static String getDay(int day) {
+        switch (day) {
+            case 1:
+                return "SUNDAY";
+            case 2:
+                return "MONDAY";
+            case 3:
+                return "TUESDAY";
+            case 4:
+                return "WEDNESDAY";
+            case 5:
+                return "THURSDAY";
+            case 6:
+                return "FRIDAY";
+            case 7:
+                return "SATURDAY";
+            default:
+                return "SHOULD NEVER HAPPEN!!";
+        }
+    }
+}
---
+++ Java/src/test/java/challenges/introduction/javadateandtime/JavaDateAndTimeTest.java
@@ -0,0 +1,29 @@
+package challenges.introduction.javadateandtime;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by nino on 3/18/17.
+ */
+public class JavaDateAndTimeTest {
+    @Test
+    public void getDayTest() {
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        String[] args = null;
+        String data = "08 05 2015";
+        String expected = "WEDNESDAY";
+
+        System.setOut(new PrintStream(stdout));
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        JavaDateAndTime.main(args);
+
+        assertEquals(expected, stdout.toString());
+    }
+}
