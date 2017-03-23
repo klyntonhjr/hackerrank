a2af14881d46367330a59a161204d83fcebcd658
Marklyton Holmes <marklytonh@gmail.com>
2017-03-22 20:28:38 -0400

    hallenge Failed: Java > String > Tag Content Extractor Seriously need to redo this one.

---
+++ Java/src/main/java/challenges/strings/tagcontentextractor/TagContentExtractor.java
@@ -0,0 +1,10 @@
+package challenges.strings.tagcontentextractor;
+
+/**
+ * Created by nino on 3/20/17.
+ */
+public class TagContentExtractor {
+    public static void main(String[] args) {
+        // NEED TO REDO DO THIS ONE
+    }
+}
