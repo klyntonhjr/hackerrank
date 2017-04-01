f6d638bab94d3bd9f9b5f7ee1f56c2041667a8e4
Marklyton Holmes <marklytonh@gmail.com>
2017-03-31 22:49:11 -0400

    Challenge Completed: Java > Object Oriented Programming > Java Inheritance II

---
+++ Java/src/main/java/challenges/oop/JavaInheritanceII.java
@@ -0,0 +1,17 @@
+package challenges.oop;
+
+/**
+ * Created by nino on 3/31/17.
+ */
+public class JavaInheritanceII {
+}
+
+class Arithmetic {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
