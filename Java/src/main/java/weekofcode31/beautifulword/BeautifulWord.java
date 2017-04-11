a9c145f3a622913e74d251882b0e9691f15bedfe
Marklyton Holmes <marklytonh@gmail.com>
2017-04-10 23:17:06 -0400

    Problem Completed: All Competetions > Week of Code 31 > Beautiful Word

---
+++ Java/src/main/java/weekofcode31/beautifulword/BeautifulWord.java
@@ -0,0 +1,25 @@
+package weekofcode31.beautifulword;
+
+import java.util.Scanner;
+
+/**
+ * Created by nino on 4/10/17.
+ */
+public class BeautifulWord {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next().toLowerCase();
+        final String VOWEL_SET = "aeiouy";
+
+        char c, cnext;
+        String outmsg = "Yes";
+        int strlen = str.length();
+        for (int i = 0; i < strlen - 1; i++) {
+            c = str.charAt(i);
+            cnext = str.charAt(i+1);
+            if(c == cnext || (VOWEL_SET.indexOf(c) != -1 && VOWEL_SET.indexOf(cnext) != -1))
+                outmsg = "No";
+        }
+        System.out.println(outmsg);
+    }
+}
---
+++ Java/src/test/java/weekofcode31/beautifulword/BeautifulWordTest.java
@@ -0,0 +1,101 @@
+package weekofcode31.beautifulword;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by nino on 4/10/17.
+ * We consider a word, , to be beautiful if the following two conditions are satisfied:
+ *
+ * No two consecutive characters are the same.
+ * No two consecutive characters are in the following vowel set: a, e, i, o, u, y. Note that we consider y to be a vowel in this challenge.
+ */
+public class BeautifulWordTest {
+    private final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+    private final String[] args = null;
+    private String data, expectedOut;
+    private final String YES = "Yes\n";
+    private final String NO = "No\n";
+
+    @Before
+    public void setUp(){
+        System.setOut(new PrintStream(stdout));
+        data = expectedOut = null;
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(null);
+        stdout.reset();
+    }
+
+    @Test
+    public void consecutiveCharsAreSameTest(){
+        data = "apple";
+        expectedOut = NO;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void consecutiveCharsAreSameButCapitalizedTest(){
+        data = "APple";
+        expectedOut = NO;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void consecutiveVowelsTest(){
+        data = "beauty";
+        expectedOut = NO;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void testCase0Test(){
+        data = "abacaba";
+        expectedOut = YES;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void testCase1Test(){
+        data = "badd";
+        expectedOut = NO;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void testCase2Test(){
+        data = "yes";
+        expectedOut = NO;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+
+    @Test
+    public void batmanTest(){
+        data = "Batman";
+        expectedOut = YES;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        BeautifulWord.main(args);
+        assertEquals(expectedOut, stdout.toString());
+    }
+}
