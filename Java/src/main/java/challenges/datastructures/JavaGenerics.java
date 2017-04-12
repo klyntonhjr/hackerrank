b297b86a17bbe06d10fb54a53cd66b3530629df4
Marklyton Holmes <marklytonh@gmail.com>
2017-04-11 20:57:04 -0400

    Challenge Update: Java > Data Structures > Java Generics Using enhanced for loop.

--- Java/src/main/java/challenges/datastructures/JavaGenerics.java
+++ Java/src/main/java/challenges/datastructures/JavaGenerics.java
@@ -29,8 +29,8 @@ public class JavaGenerics {
 class Printer
 {
     public static <T> void printArray(T[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (T a: arr) {
+            System.out.println(a);
         }
     }
 }
