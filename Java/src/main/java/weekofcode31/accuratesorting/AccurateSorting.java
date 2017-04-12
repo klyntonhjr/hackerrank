a41702c6747de7bf5689077a29cf77029874ad07
Marklyton Holmes <marklytonh@gmail.com>
2017-04-12 01:23:04 -0400

    Challenge Completed: Week of Code 31 > Accurate Sorting

---
+++ Java/src/main/java/weekofcode31/accuratesorting/AccurateSorting.java
@@ -0,0 +1,41 @@
+package weekofcode31.accuratesorting;
+
+import java.util.Scanner;
+
+/**
+ * Created by nino on 4/12/17.
+ */
+public class AccurateSorting {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            int n = in.nextInt();
+            int[] a = new int[n];
+            int x, y, temp;
+            for(int a_i=0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
+                for (int j = a_i; j > 0; j--) {
+                    x = a[j];
+                    y = a[j-1];
+                    if(x < y && Math.abs(x-y) == 1){
+                        temp = a[j];
+                        a[j] = a[j-1];
+                        a[j-1] = temp;
+                    } else
+                        break;
+                }
+            }
+
+            // Write Your Code Here
+            String isSorted = "Yes";
+            for (int i = 0; i < a.length-1; i++) {
+                if (a[i] > a[i+1]) {
+                    isSorted = "No";
+                    break;
+                }
+            }
+            System.out.println(isSorted);
+        }
+    }
+}
---
+++ Java/src/test/java/weekofcode31/accuratesorting/AccurateSortingTest.java
@@ -0,0 +1,81 @@
+package weekofcode31.accuratesorting;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by nino on 4/11/17.
+ */
+public class AccurateSortingTest {
+    private final String[] args = null;
+    private final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+    private String data, expected;
+    private int numOfTestCases;
+
+    @Before
+    public void setUp(){
+        System.setOut(new PrintStream(stdout));
+        data = expected = null;
+        numOfTestCases = 1;
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(null);
+        stdout.reset();
+    }
+
+    @Test
+    public void simpleSort(){
+        data = numOfTestCases + "\n" +
+                "2\n" +
+                "1 0";
+        expected = "Yes\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        AccurateSorting.main(args);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    public void moreThan1Sort(){
+        data = numOfTestCases + "\n" +
+                "2\n" +
+                "2 0";
+        expected = "No\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        AccurateSorting.main(args);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    public void notPossibleSort(){
+        data = numOfTestCases + "\n" +
+                "3\n" +
+                "2 1 0";
+        expected = "No\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        AccurateSorting.main(args);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    public void hackerrankCase(){
+        data = "2\n" +
+                "4\n" +
+                "1 0 3 2\n" +
+                "3\n" +
+                "2 1 0";
+        expected = "Yes\n" +
+                "No\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        AccurateSorting.main(args);
+        assertEquals(expected, stdout.toString());
+    }
+}
